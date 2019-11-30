77eab7f542d1c0c57519c7e09da96ef02a49c605
Matt Obert on Windows <matt@hotwebmatter.com>
2019-11-30 12:41:56 -0500

    Implement DisplayAllSales() method

--- Program.cs
+++ Program.cs
@@ -7,7 +7,13 @@ namespace UniversalSales
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            string[,] salesArray = { {"Collier", "2500", "35000", "20000"},
+                                {"Lee", "17000", "50000", "10000"},
+                                {"Brown", "13000", "25000", "25000"},
+                                {"Cano", "55000", "35000", "30000"} };
+            SalesMatrix mySalesMatrix = new SalesMatrix();
+            mySalesMatrix.SalesArray = salesArray;
+            WriteLine(mySalesMatrix.DisplayAllSales());
         }
     }
 }
---
+++ SalesMatrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversalSales
+{
+    class SalesMatrix
+    {
+        private string[,] salesArray;
+        
+        public string[,] SalesArray
+        {
+            set { salesArray = value; }
+        }
+
+        public string DisplayAllSales()
+        {
+            string result = String.Format("************************\n");
+            result += String.Format("Display all sales\n");
+            result += String.Format("Name\tJan\tFeb\tMar\n");
+            // Generate sales matrix
+            for (int i = 0; i < salesArray.GetLength(0); i++)
+            {
+                result += String.Format("{0}\t{1}\t{2}\t{3}\n", salesArray[i, 0], salesArray[i, 1], salesArray[i, 2], salesArray[i, 3]);
+            }
+            result += String.Format("\n************************\n");
+            return result;
+        }
+    }
+}
