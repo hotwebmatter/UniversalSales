698a49a669a1b4e4d61a1b68ad4e0fd9949f1b76
Matt Obert on Windows <matt@hotwebmatter.com>
2019-11-30 11:00:56 -0500

    Initial commit of UniversalSales project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversalSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
